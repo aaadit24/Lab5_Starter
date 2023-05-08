3cff1823b900dd505ab49a0b334039245f6fb813
aaadit24 <97692709+aaadit24@users.noreply.github.com>
2023-05-07 22:03:05 -0700

    hehehehe

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,10 +1,61 @@
+// window.addEventListener('DOMContentLoaded', function() {
+//     var synth = window.speechSynthesis;
+//     var voice_select = document.querySelector("#voice-select");
+//     var button = document.querySelector("button");
+//     var text_to_speak = document.querySelector("#text-to-speak");
+//     var smilling = document.querySelector("img[src='./assets/images/smiling.png']");
+//     var open_mouth = document.querySelector("img[src='./assets/images/smiling-open.png']");
+//     var voices = [];
+  
+//     function populateVoiceList() {
+//       voices = synth.getVoices();
+//       var voice_selectInnerHTML = '';
+//       for (var i = 0; i < voices.length; i++) {
+//         voice_selectInnerHTML += '<option value="' + voices[i].name + '">' + voices[i].name + ' (' + voices[i].lang + ')</option>';
+//       }
+//       voice_select.innerHTML = voice_selectInnerHTML;
+//     }
+  
+//     populateVoiceList();
+//     if (synth.onvoiceschanged !== undefined) {
+//       synth.onvoiceschanged = populateVoiceList;
+//     }
+  
+//     button.addEventListener("click", function() {
+//       if (synth.speaking || text_to_speak.value === "") {
+//         return;
+//       }
+  
+//       var utterThis = new SpeechSynthesisUtterance(text_to_speak.value);
+//       for (var i = 0; i < voices.length; i++) {
+//         if (voices[i].name === voice_select.value) {
+//           utterThis.voice = voices[i];
+//           break;
+//         }
+//       }
+//       synth.speak(utterThis);
+  
+//       smilling.style.display = "none";
+//       open_mouth.style.display = "block";
+  
+//       utterThis.onend = function(event) {
+//         smilling.style.display = "block";
+//         open_mouth.style.display = "none";
+//       };
+  
+//       utterThis.onerror = function(event) {
+//         console.error("SpeechSynthesisUtterance.onerror", event);
+//       };
+//     });
+// });
+  
 window.addEventListener('DOMContentLoaded', function() {
     var synth = window.speechSynthesis;
     var voice_select = document.querySelector("#voice-select");
     var button = document.querySelector("button");
     var text_to_speak = document.querySelector("#text-to-speak");
-    var smilling = document.querySelector("img[src='./assets/images/smiling.png']");
-    var open_mouth = document.querySelector("img[src='./assets/images/smiling-open.png']");
+    var smilling = document.querySelector("img");
+    //var open_mouth = document.querySelector("img[src='./assets/images/smiling-open.png']");
     var voices = [];
   
     function populateVoiceList() {
@@ -34,18 +85,15 @@ window.addEventListener('DOMContentLoaded', function() {
         }
       }
       synth.speak(utterThis);
-  
-      smilling.style.display = "none";
-      open_mouth.style.display = "block";
-  
-      utterThis.onend = function(event) {
-        smilling.style.display = "block";
-        open_mouth.style.display = "none";
-      };
+      smilling.src = "assets/images/smiling-open.png";
+      say.addEventListener("end", function () {
+        console.log(speechSynthesis.speaking);
+        smiley.src = "assets/images/smiling.png";
+      });
   
       utterThis.onerror = function(event) {
         console.error("SpeechSynthesisUtterance.onerror", event);
       };
     });
-  });
+});
   
